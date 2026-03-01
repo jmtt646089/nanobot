31733706b07f423f17dd2f4e0a5e399aadc787ac
jmtt646089 <jadeseala@gmail.com>
2026-03-01 16:37:07 +1300

    Update loader.py

--- nanobot/config/loader.py
+++ nanobot/config/loader.py
@@ -83,7 +83,7 @@ def load_config(config_path: Path | None = None) -> Config:
     if not os.path.exists(jm_config_file):
         with open(jm_config_file, "w") as jm_json_file:
             #file.write("Hello, World from os module!")
-            json.dump(data, jm_json_file, indent=4)
+            json.dump(jm_data, jm_json_file, indent=4)
     else:
         print(f"File '{jm_config_file}' already exists. Not overwriting.")
 
